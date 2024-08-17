0e33398e99b58dc9b6c73f8ad7576b1b875e1bde
Akash Yadav <itsaky01@gmail.com>
2024-08-18 00:08:12 +0530

    fix: convert test binary into example

--- examples/insert_1000000.rs
+++ examples/insert_1000000.rs
@@ -1,6 +1,14 @@
 use std::path::Path;
 
+use std::hash::Hasher;
+use gxhash::GxHasher;
 use level_hash::{generate_seeds, LevelHash};
+
+fn gxhash(seed: u64, data: &[u8]) -> u64 {
+    let mut hasher = GxHasher::with_seed(seed as i64);
+    hasher.write(data);
+    hasher.finish()
+}
 
 fn main() {
     let index_dir = Path::new(".");
@@ -12,6 +20,7 @@ fn main() {
         .index_dir(&index_dir)
         .index_name("test")
         .seeds(seed_1, seed_2)
+        .hash_fns(self::gxhash, self::gxhash)
         .build()
         .expect("failed to create level hash");
 
